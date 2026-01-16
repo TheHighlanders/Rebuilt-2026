41ff7fc5fd8874f08b2e4edb0cfc1f68891b5934
FRC-6201 <115188996+FRC-6201@users.noreply.github.com>
2026-01-16 15:56:55 -0500

    update

--- src/main/java/frc/robot/commands/Align.java
+++ src/main/java/frc/robot/commands/Align.java
@@ -5,8 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Align extends Command {
@@ -21,9 +19,7 @@ public class Align extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
