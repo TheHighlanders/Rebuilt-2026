76020590868b8f7c15d140aead0f618df2df5b0f
1203 <84973602+SiPi1@users.noreply.github.com>
2026-02-09 16:15:38 -0500

    Added encoder

--- src/main/java/frc/robot/subsystems/Intake/Deploy.java
+++ src/main/java/frc/robot/subsystems/Intake/Deploy.java
@@ -5,18 +5,21 @@
 package frc.robot.subsystems.Intake;
 
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class Deploy extends SubsystemBase {
   /** Creates a new Deploy. */
   SparkMax deployMotor = new SparkMax(IntakeConstants.DEPLOYID, MotorType.kBrushless);
+  RelativeEncoder deployEncoder = deployMotor.getEncoder();
 
   SparkMaxConfig config = new SparkMaxConfig();
 
@@ -24,21 +27,41 @@ public class Deploy extends SubsystemBase {
     config.smartCurrentLimit(50).idleMode(IdleMode.kBrake);
 
     deployMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    deployEncoder.setPosition(0);
   }
   // Adds start and stop for deploying
   public Command deployCMD() {
     // Deploys fuel
-    return runOnce(
-        () -> {
-          deployMotor.set(IntakeConstants.DEPLOY_SPEED);
-        });
+    return Commands.deadline(
+            Commands.waitUntil(() -> deployEncoder.getPosition() == IntakeConstants.DEPLOY_POSITION),
+              run(
+                () -> {
+                  deployMotor.set(IntakeConstants.DEPLOY_SPEED);
+                })
+            )
+            .andThen(
+            run(
+              () -> {
+                deployMotor.set(0);
+              }
+            ));
   }
   // Stops motor
-  public Command stopDeployCMD() {
-    return runOnce(
-        () -> {
-          deployMotor.set(0);
-        });
+  public Command undeployCMD() {
+    return Commands.deadline(
+            Commands.waitUntil(() -> deployEncoder.getPosition() == 0),
+              run(
+                () -> {
+                  deployMotor.set(0-IntakeConstants.DEPLOY_SPEED);
+                })
+            )
+            .andThen(
+            run(
+              () -> {
+                deployMotor.set(0);
+              }
+            ));
   }
 
   @Override
