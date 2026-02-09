6f80cd457a0a692b6536acbbf6db32265ae69a7b
1203 <84973602+SiPi1@users.noreply.github.com>
2026-02-09 16:35:41 -0500

    Added encoder

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -5,36 +5,54 @@
 package frc.robot.subsystems.climber;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
   SparkMax climbMotor = new SparkMax(ClimberConstants.CLIMBERID, MotorType.kBrushless);
+  RelativeEncoder climbEncoder = climbMotor.getEncoder();
   double climberspeed = 0.6;
   /** Creates a new Climber. */
-  public Climber() {}
-
-  public Command climbCMD() {
-    return runOnce(
-        () -> {
-          climbMotor.set(climberspeed);
-        });
+  public Climber() {
+    climbEncoder.setPosition(0);
   }
 
-  public Command downclimbCMD() {
-    return runOnce(
-        () -> {
-          climbMotor.set(0-climberspeed);
-        });
+  public Command raiseCMD() {
+    // Deploys fuel
+    return Commands.deadline(
+            Commands.waitUntil(() -> climbEncoder.getPosition() == ClimberConstants.UP_POSITION),
+              run(
+                () -> {
+                  climbMotor.set(ClimberConstants.RAISE_SPEED);
+                })
+            )
+            .andThen(
+            run(
+              () -> {
+                climbMotor.set(0);
+              }
+            ));
   }
 
-  public Command stopCMD() {
-    return runOnce(
-        () -> {
-          climbMotor.set(0);
-        });
+  public Command pullCMD() {
+    // Deploys fuel
+    return Commands.deadline(
+            Commands.waitUntil(() -> climbEncoder.getPosition() == 0),
+              run(
+                () -> {
+                  climbMotor.set(ClimberConstants.PULL_SPEED);
+                })
+            )
+            .andThen(
+            run(
+              () -> {
+                climbMotor.set(0);
+              }
+            ));
   }
 
   @Override
